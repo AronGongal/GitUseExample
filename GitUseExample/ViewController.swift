47ae50e17ce2605f9e3a9ae8342bbd853f47cc64
Gongal, Aron <165740@s020rmc148-16.local>
2018-11-14 09:12:01 -0600

    Initial Commit

---
+++ GitUseExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitUseExample
+//
+//  Created by Gongal, Aron on 11/14/18.
+//  Copyright © 2018 Gongal, Aron. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
